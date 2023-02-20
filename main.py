f6105b5b078201d072608e901e07d5b71204fc51
wildetesting <39714223+wildetesting@users.noreply.github.com>
2023-02-21 09:16:49 +1300

    initial basic structure for oo design in python

---
+++ main.py
@@ -0,0 +1,29 @@
+# some object oriented basic structure to work with
+# classes and objects in Python
+
+# the blueprint of objects i would like to create
+class Parrot:
+    # class attributes
+    name = ""
+    age = 0
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print("hi")
+
+    # create a few objects
+    parrot1 = Parrot()
+    parrot1.name = 'blue'
+    parrot1.age = 10
+
+    # create another object
+    parrot2 = Parrot()
+    parrot2.name = 'woo'
+    parrot2.age = 12
+
+    print(f"{parrot1.name} is {parrot1.age} years old.")
+    print(f"{parrot2.name} is {parrot2.age} years old.")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
